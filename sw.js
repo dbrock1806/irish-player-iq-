625e8aae98982d76b11487bc0d4c27136f4db482
dbrock1806 <d_brock1806@yahoo.com>
2026-08-14 16:06:06 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const C='ipiq-v6';
-const S=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./roster.json','./schedule.json','./stats.json','./history.json','./opponents.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(S))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.pathname.includes('/data/')){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))})
